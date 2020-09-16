93b66dd67d6a5cd075aa101be80c3e634bf6e829
aubelelautaro <laucha190499@gmail.com>
2020-09-16 18:03:10 -0300

    primer commit

---
+++ salaDeJuegos/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { JuegosComponent } from './components/juegos/juegos.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JugadoresComponent } from './components/jugadores/jugadores.component';
+import { ListadoJugadoresComponent } from './components/listado-jugadores/listado-jugadores.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    JuegosComponent,
+    NotFoundComponent,
+    JugadoresComponent,
+    ListadoJugadoresComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
