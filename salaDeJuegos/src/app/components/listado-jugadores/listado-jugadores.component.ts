bfb88d7dc98891529fc2780f37b3d7cc13b8270f
aubelelautaro <laucha190499@gmail.com>
2020-09-16 23:11:51 -0300

    cambios esteticos

--- salaDeJuegos/src/app/components/listado-jugadores/listado-jugadores.component.ts
+++ salaDeJuegos/src/app/components/listado-jugadores/listado-jugadores.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Jugador } from '../../clases/jugador';
 const ELEMENTOS: Jugador[] = [
-  {indice: 1, nombre: 'Hydrogen', puntaje: 10079, juego: 'Ta te ti'},
-  {indice: 2, nombre: 'Helium', puntaje: 9526, juego: 'MemoTest'},
-  {indice: 3, nombre: 'Lithium', puntaje: 4941, juego: 'asd'},
-  {indice: 4, nombre: 'Beryllium', puntaje: 2122, juego: 'asdasd'},
+  {indice: 1, nombre: 'Lautaro', puntaje: 10079, juego: 'Ta te ti'},
+  {indice: 2, nombre: 'Federico', puntaje: 9526, juego: 'MemoTest'},
+  {indice: 3, nombre: 'Agustin', puntaje: 4941, juego: 'asd'},
+  {indice: 4, nombre: 'Agustina', puntaje: 2122, juego: 'asdasd'},
 
 ];
 
