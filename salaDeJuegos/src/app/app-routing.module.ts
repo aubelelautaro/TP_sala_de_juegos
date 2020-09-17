ef56fd13496f291402ec809faf05855d11fa3d27
aubelelautaro <laucha190499@gmail.com>
2020-09-16 22:38:45 -0300

    cambios esteticos

--- salaDeJuegos/src/app/app-routing.module.ts
+++ salaDeJuegos/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { JugadoresComponent } from './components/jugadores/jugadores.component';
+import { ListadoJugadoresComponent } from './components/listado-jugadores/listado-jugadores.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'',
+  component: HomeComponent
+},
+{
+  path:'/listadoJugadores',
+  component: ListadoJugadoresComponent
+},
+{
+  path:'/jugadores',
+  component: JugadoresComponent
+},
+{
+  path:'**',
+  component: NotFoundComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
